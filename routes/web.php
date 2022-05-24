2badd4f3e038167c52951e82048626b81eb443f2
sosoww1 <kerboutechafik@gmail.com>
2022-05-24 21:08:06 +0100

    AddLoginWithRole

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
@@ -19,3 +20,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('/products', ProductController::class);
+
+Auth::routes();
+
+Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
