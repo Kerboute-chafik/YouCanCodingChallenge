5981214bd1fd83698cf8e920c3c1a3f397298bac
sosoww1 <kerboutechafik@gmail.com>
2022-05-25 10:40:32 +0100

    AddToCart_Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+import Vue from "vue";
+import VueToastr from "vue-toastr";
+Vue.use(VueToastr);
+
+Vue.component('add-to-cart-button', require('./components/AddToCart.vue').default);
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+const app = new Vue({
+    el: '#app',
+});
